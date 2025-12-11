915279323e5a6ff16b6c4b845acb0210fa653a39
digitalvottak-creator <digital.vottak@gmail.com>
2025-12-11 10:55:38 +0200

    First Commit

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-      <h1>NuxtWelcome to the homepage</h1>
-  </div>
+  <NuxtPage />
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
